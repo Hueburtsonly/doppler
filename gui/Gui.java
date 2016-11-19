f468455904f1ef392982e316e5e14240983df85e
Hueburtsonly <Hueburtsonly@users.noreply.github.com>
2016-11-19 20:54:50 +1100

    Update GUI to display sine wave + power.

--- gui/Gui.java
+++ gui/Gui.java
@@ -11,6 +11,9 @@ public class Gui extends JPanel {
 	double phase = 0; // degrees
 	double power = 0; // dBm
 
+	double freq = 2.943e9;
+	final static double c = 299792458000.0;
+
 	public static void main(String[] args) throws Exception {
 		JFrame frame = new JFrame("Doppler");
 		Gui gui = new Gui();
@@ -22,18 +25,68 @@ public class Gui extends JPanel {
 		gui.run(frame);
 	}
 
-	public Gui() {}
+	public Gui() {
+		//setOpaque(true);
+		setBackground(Color.BLACK);
+		setForeground(Color.GREEN);
+	}
 
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
+	public void paintComponent(Graphics g1) {
+		super.paintComponent(g1);
+		Graphics2D g = (Graphics2D)g1;
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setStroke(new BasicStroke(3));
+		g.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 24));
 		int width = getWidth();
 		int height = getHeight();
 		int cx = width/2;
 		int cy = height/2;
 		int r = height / 3;
-		g.drawString("Phase = " + phase, 20,20);
-		g.drawString("Power = " + power, 20,40);
-		g.drawLine(cx, cy, cx + (int)(r * Math.cos(phase / 180 * Math.PI)), cy + (int)(r * Math.sin(phase / 180 * Math.PI)));
+		int toph = 60;
+		g.drawString(String.format("Phase =%6.1f°", phase), 20,40);
+		g.drawString(String.format("Power =%6.1f dBm", power), width/2, 40);
+		//g.drawLine(cx, cy, cx + (int)(r * Math.cos(phase / 180 * Math.PI)), cy + (int)(r * Math.sin(phase / 180 * Math.PI)));
+
+		g.setColor(Color.YELLOW);
+		int et = (int)((height - 21-toph) * (20 - power) / 170.0);
+		g.fillRect(width - 70, toph+et, 70-21, height - 21-toph-et);
+		g.drawRect(width - 70, toph, 70-21, height - 21-toph);
+
+		g.setColor(Color.WHITE);
+		g.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 18));
+		int by = toph + (height-21-toph)/2;
+		int sy = (height-21-toph)/2;
+		int wavwidth = 300;
+		final int N = width - 110 - 20;
+		g.drawLine(20, by, width - 110, by);
+		g.drawLine(20, by-sy, 20, by+sy);
+		for (boolean k = false; ; k = true) {
+			// lambda = c / f
+			// mm     = mm / s  / (1/s)
+			final double stride = k ? (wavwidth * 200 / (1e12 / freq)) : (wavwidth * 100 / (c / freq));
+			final double val = k ? 200 : 100;
+			final String format  = k ? "%.0f ps" : "%.0f mm";
+			double nval = 0;
+			for (double i = stride; i < N-30; i += stride) {
+				nval += val;
+				g.drawLine(20 + (int)i, by, 20 + (int)i, by + (k ? -20 : 20));
+				g.drawString(String.format(format, nval), 20 + (int)i - 30, by + (k ? -30 : 46));
+			}
+			if (k) {
+				break;
+			}
+		}
+
+		g.setColor(new Color(255,128,0));
+
+		int xpoints[] = new int[N];
+		int ypoints[] = new int[N];
+		for (int i=0; i < N; i++) {
+			xpoints[i] = 20 + i;
+			ypoints[i] = (int)(-Math.sin((i * Math.PI / wavwidth * 2 ) + (phase / 180 * Math.PI)) * 0.8 * sy + by);
+		}
+		g.drawPolyline(xpoints, ypoints, N);
+
 	}
 
 
